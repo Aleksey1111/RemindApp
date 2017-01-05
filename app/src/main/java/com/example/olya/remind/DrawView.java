e591f27bf102c8217e3a5fa782df3a254cb89f6c
Aleksey1111 <sla1411@ukr.net>
2017-01-05 23:03:15 +0200

    canvas

--- app/src/main/java/com/example/olya/remind/DrawView.java
+++ app/src/main/java/com/example/olya/remind/DrawView.java
@@ -2,6 +2,8 @@ package com.example.olya.remind;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -9,13 +11,16 @@ import android.view.View;
  * Created by Olya on 05.01.2017.
  */
 public class DrawView extends View {
+    Paint p;
 
-    public DrawView(MainActivity mainActivity) {
-        super(mainActivity);
+    public DrawView(Context context) {
+        super(context);
+        p = new Paint(Color.GREEN);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        canvas.drawColor(Color.GREEN);
     }
 }
