549c5be70651db9748c42d0c0bde2569bcda8ae1
Aleksey1111 <sla1411@ukr.net>
2017-01-05 22:38:33 +0200

    canvas

---
+++ app/src/main/java/com/example/olya/remind/DrawView.java
@@ -0,0 +1,21 @@
+package com.example.olya.remind;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Olya on 05.01.2017.
+ */
+public class DrawView extends View {
+
+    public DrawView(MainActivity mainActivity) {
+        super(mainActivity);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
